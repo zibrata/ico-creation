f7b2172260f5f60f8f25d6b3ac18fd17498d35d7
ljeanner <jeanneret.lucile@gmail.com>
2019-01-10 17:10:35 +0100

    Truffle Migration

--- contracts/ScratchToken.sol
+++ contracts/ScratchToken.sol
@@ -1,17 +1,20 @@
 pragma solidity >=0.4.21 <0.6.0;
 
-import "./IERC20.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
-contract ScratchToken is IERC20 {
+
+contract ScratchToken is ERC20 {
+   // mapping (address => uint256) private _balances;
+   // mapping (address => mapping (address => uint256)) private _allowed;
     address public owner;
-    string public _name; 
+    string public _name ;
     
     constructor() public {
         owner = msg.sender;
         _name = "lucile";
     
     }
-
+/*
     function get_owner() public view returns(address ){
         return owner ;
     }
@@ -19,7 +22,7 @@ contract ScratchToken is IERC20 {
     function name() public view returns (string memory){
         return _name ;
     } 
-
+*/
 }
 
 
