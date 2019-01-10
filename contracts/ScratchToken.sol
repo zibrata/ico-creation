32fa7bbbaf5e1bfa8aa34d8f11ba8de9c6941ee7
ljeanner <jeanneret.lucile@gmail.com>
2019-01-10 11:14:00 +0100

    Create ERCR20 token contract
    Using Truffle and ganache - cli

---
+++ contracts/ScratchToken.sol
@@ -0,0 +1,25 @@
+pragma solidity >=0.4.21 <0.6.0;
+
+import "./IERC20.sol";
+
+contract StratchToken is IERC20 {
+    address public owner;
+    string public _name; 
+    
+    constructor() public {
+        owner = msg.sender;
+        _name = "lucile";
+    
+    }
+
+    function get_owner() public view returns(address ){
+        return owner ;
+    }
+    
+    function name() public view returns (string memory){
+        return _name ;
+    } 
+
+}
+
+
